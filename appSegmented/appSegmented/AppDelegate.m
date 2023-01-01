964452a5b402a30fb2d28f713fd4b52402ff9633
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:56:24 -0600

    Update AppDelegate.m

--- appSegmented/appSegmented/AppDelegate.m
+++ appSegmented/appSegmented/AppDelegate.m
@@ -3,7 +3,7 @@
 //  appSegmented
 //
 //  Created by Usuario invitado on 17/10/22.
-//
+////  By: RETBOT
 
 #import "AppDelegate.h"
 
@@ -12,7 +12,7 @@
 @end
 
 @implementation AppDelegate
-
+//  By: RETBOT
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -35,6 +35,6 @@
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
-
+//  By: RETBOT
 
 @end
