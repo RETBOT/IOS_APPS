df144c96be3f64b09f0c55da9a673ac68454c178
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:34:59 -0600

    Update SceneDelegate.h

--- appDiccionarioIOs_02/appDiccionarioIOs_02/SceneDelegate.h
+++ appDiccionarioIOs_02/appDiccionarioIOs_02/SceneDelegate.h
@@ -4,7 +4,7 @@
 //
 //  Created by Roberto Esquivel Troncoso on 02/10/22.
 //
-
+//  By: RETBOT
 #import <UIKit/UIKit.h>
 
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
@@ -13,3 +13,4 @@
 
 @end
 
+//  By: RETBOT
