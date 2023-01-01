630328a79820b476ef6fe4d1f06d0aaf7b87383c
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:35:04 -0600

    Update main.m

--- appDiccionariosIOs_01/appDiccionariosIOs_01/main.m
+++ appDiccionariosIOs_01/appDiccionariosIOs_01/main.m
@@ -3,7 +3,7 @@
 //  appDiccionariosIOs_01
 //
 //  Created by Roberto Esquivel Troncoso on 02/10/22.
-//
+////  By: RETBOT
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
@@ -16,3 +16,4 @@ int main(int argc, char * argv[]) {
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
+//  By: RETBOT
