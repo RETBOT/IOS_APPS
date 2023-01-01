f30977cf10efb0052d4227f0febc4d9d89ec2da9
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:23:38 -0600

    Update ViewController.h

--- JuegoCartas_V1/JuegoCartas_V1/ViewController.h
+++ JuegoCartas_V1/JuegoCartas_V1/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  JuegoCartas_V1
 //
-//  Created by Guest User on 12/10/22.
+//  Created by Guest User on 12/10/22.//  By: RETBOT
 //
 
 #import <UIKit/UIKit.h>
@@ -12,3 +12,4 @@
 
 @end
 
+//  By: RETBOT
