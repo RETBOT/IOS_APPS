f601557ea2de2a31f8050f559e76e36137d19f34
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:22:17 -0600

    Update AppDelegate.swift

--- App3D1/App3D1/AppDelegate.swift
+++ App3D1/App3D1/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  App3D1
 //
-//  Created by Usuario invitado on 02/12/22.
+//  Created by Usuario invitado on 02/12/22.//  By: RETBOT
 //
 
 import UIKit
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+    }//  By: RETBOT
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -44,6 +44,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-
+//  By: RETBOT
 }
 
