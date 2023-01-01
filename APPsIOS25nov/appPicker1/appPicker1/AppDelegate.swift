fa5894a6b6331e92ee4d5eb3393065a588716e24
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:19:57 -0600

    Update AppDelegate.swift

--- APPsIOS25nov/appPicker1/appPicker1/AppDelegate.swift
+++ APPsIOS25nov/appPicker1/appPicker1/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  appPicker1
 //
-//  Created by Usuario invitado on 25/11/22.
+//  Created by Usuario invitado on 25/11/22. //  By: RETBOT
 //
 
 import UIKit
@@ -11,7 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+//  By: RETBOT
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+//  By: RETBOT
 
 }
 
