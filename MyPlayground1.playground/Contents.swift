b9b704d245a7c4c2f84f605f102c13c8e7f2ec14
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:26:02 -0600

    Update Contents.swift

--- MyPlayground1.playground/Contents.swift
+++ MyPlayground1.playground/Contents.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 var greeting = "Hello, playground"
 var myString:String? = nil
-myString = "Hello, Swift 4!"
+myString = "Hello, Swift 4! By: RETBOT"
 if myString != nil {
     print(myString!)
 } else {
