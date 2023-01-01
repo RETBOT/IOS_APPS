0e97fa02ed142246b1e26dd339ac26cd046e2bca
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:54:24 -0600

    Update AppDelegate.swift

--- appSwiftConjuntosIOs/appSwiftConjuntosIOs/AppDelegate.swift
+++ appSwiftConjuntosIOs/appSwiftConjuntosIOs/AppDelegate.swift
@@ -4,13 +4,13 @@
 //
 //  Created by Usuario invitado on 22/11/22.
 //
-
+//  By: RETBOT
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+//  By: RETBOT
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+//  By: RETBOT
 }
 
