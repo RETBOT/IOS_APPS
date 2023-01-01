ab68ffc05c28c32df16c4673aea403e4414e6466
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2022-12-31 19:42:43 -0600

    Update AppDelegate.m

--- appGraficosStoryBoardVistas/appGraficosStoryBoardVistas/AppDelegate.m
+++ appGraficosStoryBoardVistas/appGraficosStoryBoardVistas/AppDelegate.m
@@ -4,7 +4,7 @@
 //
 //  Created by Usuario invitado on 27/10/22.
 //
-
+//  By: RETBOT
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
@@ -28,5 +28,5 @@
     return YES;
 }
 
-
+//  By: RETBOT
 @end
